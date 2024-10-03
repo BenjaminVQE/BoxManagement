486ea6b7126d00a294f7433039d59a793dc13867
Benjamin <vaique.benjamin@gmail.com>
2024-10-03 16:19:10 +0200

    fix(#6): Create model Box

---
+++ app/Models/Box.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\User;
+use App\Models\Tenant;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Box extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'surface',
+        'price',
+        'address',
+        'description'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, "user_id");
+    }
+
+    public function tenant()
+    {
+        return $this->belongsTo(Tenant::class, "tenant_id");
+    }
+}
