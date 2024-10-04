0b922c437e807b7defe51eb344a3991894e65442
Benjamin <vaique.benjamin@gmail.com>
2024-10-04 15:30:38 +0200

    Fix(#7): create BoxController

---
+++ app/Http/Controllers/BoxController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Box;
+use App\Models\Tenant;
+use Illuminate\Http\Request;
+use Illuminate\Http\RedirectResponse;
+
+class BoxController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return view('boxes.index', [
+            'boxes' => Box::all()->where('user_id', auth()->id())
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        $tenants = Tenant::all()->where('user_id', auth()->id());
+
+        return view('boxes.create', [
+            'tenants' => $tenants
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request): RedirectResponse
+    {
+        $box = new Box();
+        $box->name = $request->get('name');
+        $box->price = $request->get('price');
+        $box->surface = $request->get('surface');
+        $box->address = $request->get('address');
+        $box->tenant_id = $request->get('tenant');
+        $box->user_id = auth()->id();
+
+        $box->save();
+
+        return redirect()->route('boxes.index');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Box $box)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Box $box)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update()
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy($id)
+    {
+        $box = Box::findOrFail($id);
+        $box->delete();
+
+        return redirect()->route('boxes.index');
+    }
+}
