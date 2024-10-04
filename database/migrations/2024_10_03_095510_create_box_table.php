09161727a9e94398ef778a1d83ed2fedab37599f
Benjamin <vaique.benjamin@gmail.com>
2024-10-04 10:02:36 +0200

    fix(Box Table): Change name "box" to "boxes"

--- database/migrations/2024_10_03_095510_create_box_table.php
+++ database/migrations/2024_10_03_095510_create_box_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('box', function (Blueprint $table) {
+        Schema::create('boxes', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->integer('surface');
@@ -31,6 +31,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('box');
+        Schema::dropIfExists('boxes');
     }
 };
