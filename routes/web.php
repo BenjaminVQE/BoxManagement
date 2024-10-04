db100d21818bf0354aabe0c67a6e9df6d49730a9
Benjamin <vaique.benjamin@gmail.com>
2024-10-04 15:29:45 +0200

    fix(Routing): create Route to boxes

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,12 @@
 <?php
 
+use App\Http\Controllers\BoxController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('home');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
@@ -15,6 +16,11 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/boxes', [BoxController::class, 'index'])->name('boxes.index');
+    Route::get('/boxes/create', [BoxController::class, 'create'])->name('boxes.create');
+    Route::post('/boxes', [BoxController::class, 'store'])->name('boxes.store');
+    Route::delete('/boxes', [BoxController::class, 'destroy'])->name('boxes.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
