61c9fb54d0deb58653c34920f2b9a067ac2c4033
Benjamin <vaique.benjamin@gmail.com>
2024-10-21 16:15:53 +0200

    Add factory and seeder box

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,10 @@
 
 namespace Database\Seeders;
 
-use App\Models\Blog;
+use App\Models\Box;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use App\Models\User;
+use App\Models\Tenant;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -17,11 +18,18 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'lastName' => 'Test ',
-            'firstName' => 'User',
-            'phone' => '0678456732',
             'email' => 'test@example.com',
-            'address' => '',
+        ]);
+        Tenant::factory()->create([
+            'user_id' => 1,
+        ]);
+        Box::factory()->create([
+            'name' => 'Box de garage #1',
+            'surface' => 34,
+            'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam semper vestibulum orci, eget molestie nulla auctor ac. Aenean vel pellentesque odio, vel eleifend diam. Sed lobortis pharetra diam, non commodo massa auctor eget. Mauris ultrices ligula et rutrum pretium. Suspendisse potenti.',
+            'price' => 359,
+            'tenant_id' => 1,
+            'user_id' => 1,
         ]);
     }
 }
