2ef68c27e66ee8eafcd0f759c2b2ddff7dee3d81
Benjamin <vaique.benjamin@gmail.com>
2025-02-12 11:32:01 +0100

    Add phoneNumber to Tenants

--- database/migrations/2024_10_03_091931_create_tenants_table.php
+++ database/migrations/2024_10_03_091931_create_tenants_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('lastName');
             $table->string('firstName');
+            $table->string('phoneNumber');
             $table->unsignedBigInteger('user_id');
             $table->foreign('user_id')->references('id')->on('users');
             $table->timestamps();
