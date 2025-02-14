b72d63978d495c68f02b33077c315ee730eb4be9
Ben <vaique.benjamin@gmail.com>
2025-02-14 19:24:16 +0100

    Update name table (Status: Open. #75)

--- database/migrations/2025_02_14_145938_create_contract_table.php
+++ database/migrations/2025_02_14_145938_create_contract_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('contract', function (Blueprint $table) {
+        Schema::create('contracts', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->date('date_start');
